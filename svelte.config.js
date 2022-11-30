24905b185ce12a1e227c42a240b9704e3e86ec29
Eric Muckley <ericmuckley@gmail.com>
2022-11-30 15:13:53 -0800

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from "@sveltejs/adapter-static"; 
+// was "@sveltejs/adapter-auto"
+
+const dev = "production" === "development";
+
+/** @type {import(""@sveltejs/kit").Config} */
+const config = {
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs",
+			fallback: "index.html",
+        }),
+        paths: {
+            base: dev ? "" : "/me", // github repo name
+        },
+        // hydrate the <div id="svelte"> element in src/app.html
+        //target: "#svelte"
+    }
+};
+
+export default config;
