9ad1b25dc6e73ec38905d6ccc69bba37e148af62
Eric Muckley <ericmuckley@gmail.com>
2022-12-18 21:24:03 -0800

    additions to skills section

---
+++ docs/_app/immutable/chunks/2-54a72358.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-57ea1dbf.js";export{t as component};
--- docs/_app/immutable/chunks/2-7db4f037.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-a83fd183.js";export{t as component};
